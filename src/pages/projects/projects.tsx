a8e6f8d22f317045147e37f414361e68b801b09e
nabilahzaid <nabilah.zaid@gmail.com>
2024-08-18 03:24:13 +0800

    change project card mapping

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -29,16 +29,9 @@ function Projects(_props: Props) {
         </div>
         <div className="lg:px-48 md:px-28 max-md:px-12 mb-10">
           <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-sm:flex-1 max-sm:gap-x-10 gap-4">
-            {Array.from({ length: ProjectData.length }, (_, index) => {
-              const project = ProjectData[index];
-              return (
-                <ProjectCard
-                  dataProject={project}
-                  day={index + 1}
-                  key={index}
-                />
-              );
-            })}
+            {ProjectData.map((item, i) => (
+              <ProjectCard dataProject={item} day={i + 1} key={i} />
+            ))}
           </div>
         </div>
       </div>
